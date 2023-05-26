054c15308c7d5f97d47fe00e58441bd9e8e88ed5
Fernando Gabriel Fucks Pedro <7755287@univali.br>
2023-05-26 17:34:41 -0300

    Atualizacao, Put, Delete, Correções

--- src/Allog2405.Api/Controllers/ClientesController.cs
+++ src/Allog2405.Api/Controllers/ClientesController.cs
@@ -10,15 +10,74 @@ public class ClientesController : ControllerBase {
 
     [HttpGet]
     public ActionResult<IEnumerable<Cliente>> GetClientes() {
-        var result = ClienteData.Get().listaClientes;
+        List<Cliente> result = ClienteData.Get().listaClientes;
         return Ok(result);
     }
 
+    [HttpGet("{id:int:min(1)}", Name = "GetClientePorId")]
+    public ActionResult<Cliente> GetClientePorId([FromRoute] int id) {
+        Cliente? cliente = ClienteData.Get().listaClientes.FirstOrDefault(c => c.id == id, null);
+        return cliente != null ? Ok(cliente) : NotFound();
+    }
+
+    [HttpGet("cpf/{cpf}")]
+    public ActionResult<Cliente> GetClientePorCpf([FromRoute] string cpf) {
+        Cliente? cliente = ClienteData.Get().listaClientes.FirstOrDefault(c => c.cpf == cpf, null);
+        return cliente != null ? Ok(cliente) : NotFound();
+    }
+
     [HttpPost]
-    [Route("post")]
-    public ActionResult Post([FromBody] Cliente cliente) {
+    public ActionResult CreateCliente([FromBody] Cliente clienteBody) {
         ClienteData data = ClienteData.Get();
-        data.listaClientes.Add(cliente);
+
+        Cliente newCliente = new Cliente {
+            id = data.listaClientes.Max(c => c.id) + 1,
+            nome = clienteBody.nome,
+            cpf = clienteBody.cpf
+        };
+
+        foreach(Cliente c in data.listaClientes)
+            if (newCliente.cpf == c.cpf)
+                return Unauthorized("--CPF EXISTENTE");
+
+        data.listaClientes.Add(newCliente);
+        return CreatedAtRoute(
+            "GetClientePorId",
+            new {id = newCliente.id},
+            newCliente
+        );
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult EditCliente([FromRoute] int id, [FromBody] Cliente clienteBody) {
+        ClienteData data = ClienteData.Get();
+
+        Cliente? cliente = data.listaClientes.FirstOrDefault(n => n.id == id, null);
+        if(cliente == null) return NotFound();
+
+        Cliente newCliente = new Cliente {
+            id = data.listaClientes.Max(c => c.id) + 1,
+            nome = clienteBody.nome,
+            cpf = clienteBody.cpf
+        };
+
+        foreach(Cliente c in data.listaClientes)
+            if (newCliente.cpf == c.cpf)
+                return Unauthorized("--CPF EXISTENTE");
+
+        if(newCliente.nome != null && newCliente.nome != "") data.listaClientes.First(n => n.id == id).nome = newCliente.nome;
+        if(newCliente.cpf != null && newCliente.cpf != "") data.listaClientes.First(n => n.id == id).cpf = newCliente.cpf;
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<Cliente> DeleteClientePorId([FromRoute] int id) {
+        ClienteData data = ClienteData.Get();
+
+        Cliente? cliente = data.listaClientes.FirstOrDefault(n => n.id == id, null);
+        if (cliente == null) return NotFound();
+
+        data.listaClientes.Remove(cliente);
         return Ok();
     }
 }
